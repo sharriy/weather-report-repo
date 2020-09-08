21a5f41449a0edb2030a3f99a5a986a7fcacc14f
Riya Sharma <sharmariya8497@gmail.com>
2020-09-09 03:55:15 +0530

    Adding rest api (list of cities)

--- Vagrantfile
+++ Vagrantfile
@@ -15,7 +15,7 @@ Vagrant.configure("2") do |config|
  config.vm.box = "ubuntu/bionic64"
  config.vm.box_version = "~> 20200304.0.0"
 
- config.vm.network "forwarded_port", guest: 8000, host: 1234
+ config.vm.network "forwarded_port", guest: 8000, host: 8000
 
  config.vm.provision "shell", inline: <<-SHELL
    systemctl disable apt-daily.service
--- weather_api/views.py
+++ weather_api/views.py
@@ -1,18 +1,33 @@
 #from django.shortcuts import render
 from rest_framework.response import Response
+from .models import City
 
 def index(request):
 
+
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=0871a0f9691204fa0330e5caf06aae69'
-    city = 'Delhi'
-    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
 
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon']
-    }
+    cities = City.objects.all() #return all the cities in the database
 
-    return Response({'weather' : weather})
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
+
+    return Response({'weather_data' : weather_data, 'form' : form})
 #    return render(request, 'weather/index.html', context) #returns the index.html template
