ed9d212c69e6d55cb1c283659a987157d6fdc69c
Riya Sharma <sharmariya8497@gmail.com>
2020-09-08 16:14:45 +0530

    ading rest api code 1

--- weather_api/views.py
+++ weather_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=0871a0f9691204fa0330e5caf06aae69'
+    city = 'Delhi'
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather' : weather}
+
+    return render(request, 'weather/index.html', context) #returns the index.html template
